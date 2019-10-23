2212d8e26f567fcab45a18fb58057798b421db42
vignesh <vigneshit13@gmail.com>
2019-10-23 23:44:03 +0530

    Code Cleaning

--- src/Home.js
+++ src/Home.js
@@ -47,6 +47,56 @@ class Home extends Component {
     )
   }
 
+  renderCategorySelector = () => {
+    return(
+      <div className="form-group">
+        <label>Name:</label>
+        <input className="form-control" id="name" onChange={this.onNameChange} placeholder='Enter your name...'></input>
+        <label>Select category:</label>
+        <select className="form-control" onChange={this.onSelectCategory}>
+          <option value='9'>General Knowledge</option>
+          <option value='10'>Books</option>
+          <option value='18'>Computers</option>
+          <option value='21'>Sports</option>
+          <option value='22'>Geography</option>
+          <option value='23'>History</option>
+          <option value='24'>Politics</option>
+          <option value='17'>Nature</option>
+          <option value='26'>Celebrities</option>
+          <option value='27'>Animals</option>
+          <option value='28'>Vehicles</option>
+        </select>
+      </div>
+    )
+  }
+
+  renderQuestionsCountSelector = () => {
+    return(
+      <div className="form-group">
+        <label>Select number of questions:</label>
+        <select className="form-control" onChange={this.onSelectNoOfQuestions} >
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={15}>15</option>
+          <option value={20}>20</option>
+        </select>
+      </div>
+    )
+  }
+
+  renderDifficultyLevelSelector = () => {
+    return(
+      <div className="form-group">
+        <label>Select difficulty level:</label>
+        <select className="form-control" onChange={this.onSelectDifficultyLevel}>
+          <option value='easy'>Easy</option>
+          <option value='medium'>Medium</option>
+          <option value='hard'>Hard</option>
+        </select>
+      </div>
+    )
+  }
+
   renderHomePage = () => {
     const { showAlert } = this.state;
     return(
@@ -56,44 +106,18 @@ class Home extends Component {
             <h1>Quizzer</h1>          
           </div>
           <div className="card-body">
-          <div className="form-group">
-            <label>Name:</label>
-            <input className="form-control" id="name" onChange={this.onNameChange} placeholder='Enter your name...'></input>
-            <label>Select category:</label>
-            <select className="form-control" onChange={this.onSelectCategory}>
-              <option value='9'>General Knowledge</option>
-              <option value='10'>Books</option>
-              <option value='18'>Computers</option>
-              <option value='21'>Sports</option>
-              <option value='22'>Geography</option>
-              <option value='23'>History</option>
-              <option value='24'>Politics</option>
-              <option value='17'>Nature</option>
-              <option value='26'>Celebrities</option>
-              <option value='27'>Animals</option>
-              <option value='28'>Vehicles</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Select number of questions:</label>
-            <select className="form-control" onChange={this.onSelectNoOfQuestions} >
-              <option value={5}>5</option>
-              <option value={10}>10</option>
-              <option value={15}>15</option>
-              <option value={20}>20</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Select difficulty level:</label>
-            <select className="form-control" onChange={this.onSelectDifficultyLevel}>
-              <option value='easy'>Easy</option>
-              <option value='medium'>Medium</option>
-              <option value='hard'>Hard</option>
-            </select>
-          </div>
+          {this.renderCategorySelector()}
+          {this.renderQuestionsCountSelector()}
+          {this.renderDifficultyLevelSelector()}
           </div>
           <div className='custom-button'>
-            <button type='button' className='btn  btn-outline-info' onClick={this.onChangeToQuizPage}>Take Quiz</button>
+            <button
+              type='button'
+              className='btn btn-outline-info'
+              onClick={this.onChangeToQuizPage}
+            >
+              Take Quiz
+            </button>
           </div>
           {showAlert ? this.renderFormIncompleteAlert() : null}
         </div>
